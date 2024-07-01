62f821623cc6413c809b1670829f19cfa47a6771
najmul-islam <najmulislam519@gmail.com>
2024-07-01 18:02:00 +0600

    change file name, add function and add seen and read field

--- backend/controllers/messageController.js
+++ backend/controllers/messageController.js
@@ -2,7 +2,7 @@ const asyncHandler = require("express-async-handler");
 const Message = require("../models/messageModel");
 const Conversation = require("../models/conversationModel");
 
-const allMessages = asyncHandler(async (req, res) => {
+const getAllMessages = asyncHandler(async (req, res) => {
   const userId = req.user._id;
   const partnerId = req.params.partnerId;
 
@@ -12,11 +12,46 @@ const allMessages = asyncHandler(async (req, res) => {
       { $and: [{ sender: partnerId }, { receiver: userId }] },
     ],
   })
-    .populate("sender", "_id name avatar")
-    .populate("receiver", "_id name avatar");
+    .populate("sender", "_id name avatar seen read")
+    .populate("receiver", "_id name avatar seen read");
 
   res.status(200).json(messages);
 });
+
+const getUnseenMessage = asyncHandler(async (req, res) => {
+  const userId = req.user._id;
+
+  const unseenMessage = await Message.countDocuments({
+    receiver: userId,
+    seen: false,
+  });
+  res.status(200).json({ unseenMessage });
+});
+
+const updateUnseenMessage = asyncHandler(async (req, res) => {
+  const userId = req.user._id;
+
+  await Message.updateMany({ receiver: userId, seen: false }, { seen: true });
+
+  // const messages = await Message.find({
+  //   $or: [
+  //     { $and: [{ sender: userId }, { receiver: partnerId }] },
+  //     { $and: [{ sender: partnerId }, { receiver: userId }] },
+  //   ],
+  // })
+  //   .populate("sender", "_id name avatar")
+  //   .populate("receiver", "_id name avatar");
+
+  const unseenMessage = await Message.countDocuments({
+    receiver: userId,
+    seen: false,
+  });
+  res.status(200).json({ unseenMessage });
+});
+
+// const readMessage = asyncHandler(async(req, res) =>{
+//   const
+// })
 
 const createMessage = asyncHandler(async (req, res) => {
   const userId = req.user._id;
@@ -29,6 +64,7 @@ const createMessage = asyncHandler(async (req, res) => {
 
   if (!conversation) {
     res.status(404);
+
     throw new Error("Conversation not found");
   }
 
@@ -38,6 +74,8 @@ const createMessage = asyncHandler(async (req, res) => {
     receiver: partnerId,
     text,
     createdAt,
+    seen: false,
+    read: false,
   });
 
   conversation.lastMessage = newMessage._id;
@@ -65,4 +103,9 @@ const createMessage = asyncHandler(async (req, res) => {
   res.status(200).json(newMessage);
 });
 
-module.exports = { allMessages, createMessage };
+module.exports = {
+  getAllMessages,
+  getUnseenMessage,
+  updateUnseenMessage,
+  createMessage,
+};
