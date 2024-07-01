c54af5c4f2e6f2ff55b171877a67cbb6c6e0e9b3
najmul-islam <najmulislam519@gmail.com>
2024-07-01 18:02:18 +0600

    add read and seen filed and route

--- backend/models/messageModel.js
+++ backend/models/messageModel.js
@@ -1,23 +1,34 @@
 const mongoose = require("mongoose");
 
-const messageSchema = mongoose.Schema({
-  sender: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
+const messageSchema = mongoose.Schema(
+  {
+    sender: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    receiver: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    text: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    seen: {
+      type: Boolean,
+      default: false,
+    },
+    read: {
+      type: Boolean,
+      default: false,
+    },
   },
-  receiver: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-  },
-  text: {
-    type: String,
-    trim: true,
-    required: true,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Message", messageSchema);
