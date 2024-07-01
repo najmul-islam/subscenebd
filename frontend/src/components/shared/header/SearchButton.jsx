f50654e674849d64c0c43ac288156814fc0cc202
najmul-islam <najmulislam519@gmail.com>
2024-07-02 01:22:57 +0600

    change color

--- frontend/src/components/shared/header/SearchButton.jsx
+++ frontend/src/components/shared/header/SearchButton.jsx
@@ -3,7 +3,10 @@ import { CiSearch } from "react-icons/ci";
 
 const SearchButton = ({ handleShowSearchBox }) => {
   return (
-    <IconButton onClick={() => handleShowSearchBox(true)}>
+    <IconButton
+      sx={{ color: (theme) => theme.palette.text.primary }}
+      onClick={() => handleShowSearchBox(true)}
+    >
       <CiSearch style={{ fontSize: "25px", display: { sm: "none" } }} />
     </IconButton>
   );
