7080c397349df28c90327c352660ff1aeaf20c60
najmul-islam <najmulislam519@gmail.com>
2024-07-02 11:24:15 +0600

    center items

--- frontend/src/components/public/subtitles/SubtitleItemSkeleton.jsx
+++ frontend/src/components/public/subtitles/SubtitleItemSkeleton.jsx
@@ -5,7 +5,7 @@ const SubtitleItemSkeleton = () => {
     <Grid item>
       <Card
         sx={{
-          width: { xs: "138px", sm: "160px" },
+          width: { xs: "160px", sm: "160px" },
           height: { xs: "271px", sm: "290px" },
         }}
       >
